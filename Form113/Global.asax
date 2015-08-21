476963fe45fffa0259397bb6f68b10ca1233a2e1
Daniel Lucazeau <contact@dlucazeau.fr>
2015-08-21 10:51:43 +0200

    Initialisation de la solution

---
+++ Form113/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Form113.MvcApplication" Language="C#" %>
---
+++ Form113/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Form113.Startup))]
+namespace Form113
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
