d7abdbd4f3ef6a7cefcfa4a137436351c216d13c
Daniel Lucazeau <contact@dlucazeau.fr>
2015-08-22 16:47:27 +0200

    Mise à niveau EF vers 6.1.3

--- DataLayer/Class1.cs
+++ DataLayer/Class1.cs
@@ -1,12 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace DataLayer
-{
-    public class Class1
-    {
+﻿namespace DataLayer {
+    public class Class1 {
     }
 }
