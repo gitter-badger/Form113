f08a35a0ffc067b82547fe4328007dc3baac45e9
Rémi LEBEGUE <remilebegue@hotmail.fr>
2015-08-24 16:19:54 +0200

    Création des Helpers
    Entity frmaeworkd manquand donc Helper Carrousel en attente

---
+++ Form113/Helper/FormFieldset.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Form113.Helper
+{
+    public class FormFieldset : IDisposable
+    {
+        private HtmlHelper _helper;
+
+        public FormFieldset(HtmlHelper helper, string legend)
+        {
+            _helper = helper;
+            _helper.ViewContext.Writer.Write("<fieldset>");
+            _helper.ViewContext.Writer.Write("<legend>{0}</legend>", legend);
+        }
+        public void Dispose()
+        {
+            _helper.ViewContext.Writer.Write("</fieldset>");
+        }
+    }
+}
