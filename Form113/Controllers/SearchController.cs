5d6cfe45e89ed1b29b6850dc19d27ca65083f97a
Yoann THERY <yoann.thery@gmail.com>
2015-08-24 18:02:31 +0200

    divers debuts searchcontroller/modif script base
    script base en cours, recherche en cours

---
+++ Form113/Controllers/SearchController.cs
@@ -0,0 +1,25 @@
+﻿using Form113.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Form113.Controllers
+{
+    public class SearchController : Controller
+    {
+        // GET: Search
+        public ActionResult Index()
+        {
+            var db = new BestArtEntities();
+            SearchViewModel svm = new SearchViewModel();
+            svm.CatSousCat = db.SousCategories.OrderBy(c => c.Nom)
+                .ToDictionary(c => c.Nom,
+                r => r.Categories.OrderBy(d => d.Nom)
+                    .ToDictionary(d => d.NumDep, d => d.Nom)
+                    );
+            return View(svm);
+        }
+    }
+}
---
+++ Form113/Models/SearchViewModel.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Xml.Serialization;
+
+namespace Form113.Models
+{
+    public class SearchViewModel
+    {
+        public int PrixMini, PrixMaxi;
+        public int[] idCategories;
+        [XmlIgnore]
+        public Dictionary<string, Dictionary<string, string>> CatSousCat { get; set; }
+
+
+
+
+        public string SerializeSearchViewModel()
+        {
+            var xmlSerializer = new XmlSerializer(typeof(SearchViewModel));
+            var writer = new StringWriter();
+            xmlSerializer.Serialize(writer, this);
+            return writer.ToString();
+        }
+
+        public static SearchViewModel UnserializeSearchViewModel(string svms)
+        {
+            var xmlSerializer = new XmlSerializer(typeof(SearchViewModel));
+            var reader = new StringReader(svms);
+            return (SearchViewModel)xmlSerializer.Deserialize(reader);
+        }
+    }
+}
