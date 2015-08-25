1e52e67d83e29fea6b901f8cf3607c7849c14084
Yoann THERY <yoann.thery@gmail.com>
2015-08-25 16:20:30 +0200

    ajout vue partielle nombre de produits
    avec un produitcontroller pour le contenir.

---
+++ Form113/Controllers/ProduitController.cs
@@ -0,0 +1,39 @@
+﻿using DataLayer.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Form113.Controllers
+{
+    public class ProduitController : BestArtController
+    {
+        // GET: Produits
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [ChildActionOnly]
+        public PartialViewResult NbProduits()
+        {
+            var db = new BestArtEntities();
+
+            var nbproduits = db.Produits.Count();
+
+            return PartialView("_NbProduits", nbproduits);
+        }
+        
+        [ChildActionOnly]
+        public PartialViewResult HighlightedProduct()
+        {
+            var db = new BestArtEntities();
+
+            var nbproduits = db.Produits.Where(p=>(p.Promotion!=0).O;
+
+            return PartialView("_NbProduits", nbproduits);
+        }
+        HighlightedProduct
+    }
+}
