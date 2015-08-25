078224f6f1ef9805039056557e981c499b3c003e
Rémi LEBEGUE <remilebegue@hotmail.fr>
2015-08-25 16:19:44 +0200

    SearchOption Continent Region Pays

---
+++ Form113/Infrastructure/SearchArt/Search.cs
@@ -0,0 +1,33 @@
+﻿using DataLayer.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Form113.Infrastructure.SearchArt
+{
+    #region UsingReg
+    using System.Collections.Generic;
+    using DataLayer.Models;
+    using Form113.Infrastructure.SearchArt.Base;
+    #endregion
+
+    public class Search : SearchBase 
+    {
+        public Search()
+        {
+            SearchAnnonces = new BestArtEntities().Produits;
+        }
+
+
+        public Search(IEnumerable<Produits> result)
+        {
+            SearchAnnonces = result;
+        }
+
+        public override IEnumerable<Produits> GetResult()
+        {
+            return SearchAnnonces;
+        }
+    }
+}
