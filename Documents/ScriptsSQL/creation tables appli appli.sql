5088dd220ccf88ebd8b822faca39d5549a4f2c67
Rémi LEBEGUE <remilebegue@hotmail.fr>
2015-08-28 16:19:46 +0200

    Commande OK et modif sql creation

--- Documents/ScriptsSQL/creation tables appli appli.sql
+++ Documents/ScriptsSQL/creation tables appli appli.sql
@@ -229,89 +229,94 @@ CONSTRAINT [Idx_Commandes_details] PRIMARY KEY CLUSTERED
 
 GO
 SET ANSI_PADDING OFF
+
 GO
+SET IDENTITY_INSERT [dbo].[Categories] ON 
 
+INSERT [dbo].[Categories] ([IdCategorie], [Libelle], [Photo]) VALUES (1, N'Arts de la table', N'ArtDeLaTable.jpg')
+INSERT [dbo].[Categories] ([IdCategorie], [Libelle], [Photo]) VALUES (2, N'Décorations', N'Decoration.jpg')
+INSERT [dbo].[Categories] ([IdCategorie], [Libelle], [Photo]) VALUES (3, N'Bijoux Ethnique', N'bijoux.jpg')
+INSERT [dbo].[Categories] ([IdCategorie], [Libelle], [Photo]) VALUES (4, N'Bagagerie', N'sac.jpg')
+INSERT [dbo].[Categories] ([IdCategorie], [Libelle], [Photo]) VALUES (5, N'Bien-être', N'entete-bienetre.jpg')
+INSERT [dbo].[Categories] ([IdCategorie], [Libelle], [Photo]) VALUES (6, N'Mode', N'mode.jpg')
+INSERT [dbo].[Categories] ([IdCategorie], [Libelle], [Photo]) VALUES (7, N'Jeux', N'jeux.jpg')
+INSERT [dbo].[Categories] ([IdCategorie], [Libelle], [Photo]) VALUES (8, N'Bazar', N'bazar.jpg')
+SET IDENTITY_INSERT [dbo].[Categories] OFF
+SET IDENTITY_INSERT [dbo].[SousCategories] ON 
 
+INSERT [dbo].[SousCategories] ([IdSousCategorie], [Nom], [IdCategorie], [Photo]) VALUES (1, N'Services à thé                ', 1, NULL)
+INSERT [dbo].[SousCategories] ([IdSousCategorie], [Nom], [IdCategorie], [Photo]) VALUES (2, N'Plateaux                      ', 1, NULL)
+INSERT [dbo].[SousCategories] ([IdSousCategorie], [Nom], [IdCategorie], [Photo]) VALUES (3, N'Plats                         ', 1, NULL)
+INSERT [dbo].[SousCategories] ([IdSousCategorie], [Nom], [IdCategorie], [Photo]) VALUES (4, N'Bols et tasses                ', 1, NULL)
+INSERT [dbo].[SousCategories] ([IdSousCategorie], [Nom], [IdCategorie], [Photo]) VALUES (5, N'Verres                        ', 1, NULL)
+INSERT [dbo].[SousCategories] ([IdSousCategorie], [Nom], [IdCategorie], [Photo]) VALUES (6, N'Assiettes                     ', 1, NULL)
+INSERT [dbo].[SousCategories] ([IdSousCategorie], [Nom], [IdCategorie], [Photo]) VALUES (7, N'Couvert                       ', 1, NULL)
+INSERT [dbo].[SousCategories] ([IdSousCategorie], [Nom], [IdCategorie], [Photo]) VALUES (8, N'Linge de table                ', 1, NULL)
+INSERT [dbo].[SousCategories] ([IdSousCategorie], [Nom], [IdCategorie], [Photo]) VALUES (9, N'Accessoires                   ', 1, NULL)
+INSERT [dbo].[SousCategories] ([IdSousCategorie], [Nom], [IdCategorie], [Photo]) VALUES (10, N'Corbeilles                    ', 2, NULL)
+INSERT [dbo].[SousCategories] ([IdSousCategorie], [Nom], [IdCategorie], [Photo]) VALUES (11, N'Vases                         ', 2, NULL)
+INSERT [dbo].[SousCategories] ([IdSousCategorie], [Nom], [IdCategorie], [Photo]) VALUES (12, N'Boîtes                        ', 2, NULL)
+INSERT [dbo].[SousCategories] ([IdSousCategorie], [Nom], [IdCategorie], [Photo]) VALUES (13, N'Bureau et papeterie           ', 2, NULL)
+INSERT [dbo].[SousCategories] ([IdSousCategorie], [Nom], [IdCategorie], [Photo]) VALUES (14, N'Petit mobilier                ', 2, NULL)
+INSERT [dbo].[SousCategories] ([IdSousCategorie], [Nom], [IdCategorie], [Photo]) VALUES (15, N'Tentures                      ', 2, NULL)
+INSERT [dbo].[SousCategories] ([IdSousCategorie], [Nom], [IdCategorie], [Photo]) VALUES (16, N'Statues et figurines          ', 2, NULL)
+INSERT [dbo].[SousCategories] ([IdSousCategorie], [Nom], [IdCategorie], [Photo]) VALUES (17, N'Décorations de Noël           ', 2, NULL)
+INSERT [dbo].[SousCategories] ([IdSousCategorie], [Nom], [IdCategorie], [Photo]) VALUES (18, N'Colliers et médailles         ', 3, NULL)
+INSERT [dbo].[SousCategories] ([IdSousCategorie], [Nom], [IdCategorie], [Photo]) VALUES (19, N'Broches et barrettes          ', 3, NULL)
+INSERT [dbo].[SousCategories] ([IdSousCategorie], [Nom], [IdCategorie], [Photo]) VALUES (20, N'Bagues                        ', 3, NULL)
+INSERT [dbo].[SousCategories] ([IdSousCategorie], [Nom], [IdCategorie], [Photo]) VALUES (21, N'Bracelets                     ', 3, NULL)
+INSERT [dbo].[SousCategories] ([IdSousCategorie], [Nom], [IdCategorie], [Photo]) VALUES (22, N'Boucles d''oreilles            ', 3, NULL)
+INSERT [dbo].[SousCategories] ([IdSousCategorie], [Nom], [IdCategorie], [Photo]) VALUES (23, N'Accessoires                   ', 3, NULL)
+INSERT [dbo].[SousCategories] ([IdSousCategorie], [Nom], [IdCategorie], [Photo]) VALUES (24, N'Sacs et paniers               ', 4, NULL)
+INSERT [dbo].[SousCategories] ([IdSousCategorie], [Nom], [IdCategorie], [Photo]) VALUES (25, N'Maroquinerie                  ', 4, NULL)
+INSERT [dbo].[SousCategories] ([IdSousCategorie], [Nom], [IdCategorie], [Photo]) VALUES (26, N'Trousses et étuis             ', 4, NULL)
+INSERT [dbo].[SousCategories] ([IdSousCategorie], [Nom], [IdCategorie], [Photo]) VALUES (27, N'Porte-clefs                   ', 4, NULL)
+INSERT [dbo].[SousCategories] ([IdSousCategorie], [Nom], [IdCategorie], [Photo]) VALUES (28, N'Bougies                       ', 5, NULL)
+INSERT [dbo].[SousCategories] ([IdSousCategorie], [Nom], [IdCategorie], [Photo]) VALUES (29, N'Photophore et bougeoirs       ', 5, NULL)
+INSERT [dbo].[SousCategories] ([IdSousCategorie], [Nom], [IdCategorie], [Photo]) VALUES (30, N'Encens et supports            ', 5, NULL)
+INSERT [dbo].[SousCategories] ([IdSousCategorie], [Nom], [IdCategorie], [Photo]) VALUES (31, N'Bien-être                     ', 5, NULL)
+INSERT [dbo].[SousCategories] ([IdSousCategorie], [Nom], [IdCategorie], [Photo]) VALUES (32, N'Mode femme                    ', 6, NULL)
+INSERT [dbo].[SousCategories] ([IdSousCategorie], [Nom], [IdCategorie], [Photo]) VALUES (33, N'Mode homme                    ', 6, NULL)
+INSERT [dbo].[SousCategories] ([IdSousCategorie], [Nom], [IdCategorie], [Photo]) VALUES (34, N'Mode enfant                   ', 6, NULL)
+INSERT [dbo].[SousCategories] ([IdSousCategorie], [Nom], [IdCategorie], [Photo]) VALUES (35, N'Echarpes et etoles            ', 6, NULL)
+INSERT [dbo].[SousCategories] ([IdSousCategorie], [Nom], [IdCategorie], [Photo]) VALUES (36, N'Accessoires                   ', 6, NULL)
+INSERT [dbo].[SousCategories] ([IdSousCategorie], [Nom], [IdCategorie], [Photo]) VALUES (37, N'Jeux                          ', 7, NULL)
+INSERT [dbo].[SousCategories] ([IdSousCategorie], [Nom], [IdCategorie], [Photo]) VALUES (38, N'Jouets en bois                ', 7, NULL)
+INSERT [dbo].[SousCategories] ([IdSousCategorie], [Nom], [IdCategorie], [Photo]) VALUES (39, N'Instruments                   ', 7, NULL)
+INSERT [dbo].[SousCategories] ([IdSousCategorie], [Nom], [IdCategorie], [Photo]) VALUES (40, N'Poupées et animaux            ', 7, NULL)
+INSERT [dbo].[SousCategories] ([IdSousCategorie], [Nom], [IdCategorie], [Photo]) VALUES (41, N'Mobiles                       ', 7, NULL)
+INSERT [dbo].[SousCategories] ([IdSousCategorie], [Nom], [IdCategorie], [Photo]) VALUES (42, N'Décoration                    ', 7, NULL)
+INSERT [dbo].[SousCategories] ([IdSousCategorie], [Nom], [IdCategorie], [Photo]) VALUES (43, N'Divers                        ', 8, NULL)
+SET IDENTITY_INSERT [dbo].[SousCategories] OFF
+SET IDENTITY_INSERT [dbo].[Produits] ON 
 
-SET IDENTITY_INSERT Categories ON
-INSERT [dbo].[Categories] ([idCategorie],[Libelle]) VALUES (1,'Arts de la table') 
-INSERT [dbo].[Categories] ([idCategorie],[Libelle]) VALUES (2,'Décorations')
-INSERT [dbo].[Categories] ([idCategorie],[Libelle]) VALUES (3,'Bijoux Ethnique')
-INSERT [dbo].[Categories] ([idCategorie],[Libelle]) VALUES (4,'Bagagerie')
-INSERT [dbo].[Categories] ([idCategorie],[Libelle]) VALUES (5,'Bien-être')
-INSERT [dbo].[Categories] ([idCategorie],[Libelle]) VALUES (6,'Mode')
-INSERT [dbo].[Categories] ([idCategorie],[Libelle]) VALUES (7,'Jeux')
-INSERT [dbo].[Categories] ([idCategorie],[Libelle]) VALUES (8,'Bazar')
-SET IDENTITY_INSERT Categories OFF
+INSERT [dbo].[Produits] ([IdProduit], [Nom], [Couleur], [Description], [Prix], [IdSousCategorie], [DateMiseEnVente], [Promotion], [MisEnAvant], [CodePays], [Stock], [NbVues]) VALUES (1, N'cacao', N'bleu', N'Restabat ut Caesar post haec properaret accitus et abstergendae causa suspicionis sororem suam, eius uxorem, Constantius ad se tandem desideratam venire multis fictisque blanditiis hortabatur. quae licet ambigeret metuens saepe cruentum, spe tamen quod eum lenire poterit ut germanum profecta, cum Bithyniam introisset, in statione quae Caenos Gallicanos appellatur, absumpta est vi febrium repentina. cuius post obitum maritus contemplans cecidisse fiduciam qua se fultum existimabat, anxia cogitatione, quid moliretur haerebat.', 500, 1, CAST(N'1984-10-15 00:00:00.000' AS DateTime), 0.2, 1, N'AIA', 50, NULL)
+INSERT [dbo].[Produits] ([IdProduit], [Nom], [Couleur], [Description], [Prix], [IdSousCategorie], [DateMiseEnVente], [Promotion], [MisEnAvant], [CodePays], [Stock], [NbVues]) VALUES (2, N'Théière Coeurs', N'bleu', N'Véritable objet de collection du commerce équitable, cette théière coeur est confectionnée de manière artisanale par les artisans de l''association for craft producers, qui officie au Népal depuis 1984 et dont l''objectif est d''améliorer les conditions de vie des artisans en visant plus partiulièrement les femmes.  Fabriqué à partir de céramique.  Information utile : ce produit est adapté au lave-vaiselle et au micro-ondes !', 43, 1, CAST(N'1984-10-15 00:00:00.000' AS DateTime), 1, 0, N'NPL', 50, NULL)
+INSERT [dbo].[Produits] ([IdProduit], [Nom], [Couleur], [Description], [Prix], [IdSousCategorie], [DateMiseEnVente], [Promotion], [MisEnAvant], [CodePays], [Stock], [NbVues]) VALUES (3, N'Théière Sen', N'Blanc', N'Céramique. Turquoise et blanc. H 15 x 19cm Lavage à la main. Non adapté au micro-ondes...', 30, 1, CAST(N'1984-10-10 00:00:00.000' AS DateTime), 1, 0, N'VNM', 500, NULL)
+INSERT [dbo].[Produits] ([IdProduit], [Nom], [Couleur], [Description], [Prix], [IdSousCategorie], [DateMiseEnVente], [Promotion], [MisEnAvant], [CodePays], [Stock], [NbVues]) VALUES (4, N'Service à thé', N'blanc Rose', N'Bois d''alstonia (service 10 pièces), eucalyptus (plateau) et coton (2 serviettes) Plateau: 34 x 25cm. Service: approx: 5 x 4cm.', 40, 1, CAST(N'1984-10-10 00:00:00.000' AS DateTime), 1, 0, N'EST', 500, NULL)
+INSERT [dbo].[Produits] ([IdProduit], [Nom], [Couleur], [Description], [Prix], [IdSousCategorie], [DateMiseEnVente], [Promotion], [MisEnAvant], [CodePays], [Stock], [NbVues]) VALUES (5, N'Théière blanc et noir', N'blanc noir', N'Diam. 15 x H14/16cm, contenance 1 litre Micro-ondes et laive-vaisselle déconseillé Céramique...', 13, 1, CAST(N'1984-10-15 00:00:00.000' AS DateTime), 1, 0, N'AUS', 10, NULL)
+INSERT [dbo].[Produits] ([IdProduit], [Nom], [Couleur], [Description], [Prix], [IdSousCategorie], [DateMiseEnVente], [Promotion], [MisEnAvant], [CodePays], [Stock], [NbVues]) VALUES (6, N'Théière Dao', N'blanc', N'Céramique.Blanc et pois multicolores. Diam 11 x H13cm. Capacité 0,7l Non-adapté au lave-vaisselle', 40, 1, CAST(N'1984-10-15 00:00:00.000' AS DateTime), 1, 0, N'CHN', 500, NULL)
+INSERT [dbo].[Produits] ([IdProduit], [Nom], [Couleur], [Description], [Prix], [IdSousCategorie], [DateMiseEnVente], [Promotion], [MisEnAvant], [CodePays], [Stock], [NbVues]) VALUES (8, N'Passoire à thé en bambou', NULL, N'Passoire à thé en bambou - Inde', 4, 1, CAST(N'1984-10-15 00:00:00.000' AS DateTime), 1, 0, N'IND', 500, NULL)
+INSERT [dbo].[Produits] ([IdProduit], [Nom], [Couleur], [Description], [Prix], [IdSousCategorie], [DateMiseEnVente], [Promotion], [MisEnAvant], [CodePays], [Stock], [NbVues]) VALUES (9, N'Théière Aube rouge', N'blanc', N'Théière Aube rouge - Ceramique - Vietnam', 28, 1, CAST(N'1984-10-10 00:00:00.000' AS DateTime), 1, 0, N'VNM', 500, NULL)
+INSERT [dbo].[Produits] ([IdProduit], [Nom], [Couleur], [Description], [Prix], [IdSousCategorie], [DateMiseEnVente], [Promotion], [MisEnAvant], [CodePays], [Stock], [NbVues]) VALUES (10, N'Théière Aube rouge', N'blanc', N'Théière Aube rouge - Ceramique - Vietnam', 28, 1, CAST(N'1984-10-10 00:00:00.000' AS DateTime), 1, 0, N'VNM', 500, NULL)
+SET IDENTITY_INSERT [dbo].[Produits] OFF
+SET IDENTITY_INSERT [dbo].[Photos] ON 
 
-
-INSERT [dbo].[SousCategories] ([idCategorie],[nom]) VALUES (1,'Services à thé')
-INSERT [dbo].[SousCategories] ([idCategorie],[nom]) VALUES (1,'Plateaux')
-INSERT [dbo].[SousCategories] ([idCategorie],[nom]) VALUES (1,'Plats')
-INSERT [dbo].[SousCategories] ([idCategorie],[nom]) VALUES (1,'Bols et tasses')
-INSERT [dbo].[SousCategories] ([idCategorie],[nom]) VALUES (1,'Verres')
-INSERT [dbo].[SousCategories] ([idCategorie],[nom]) VALUES (1,'Assiettes')
-INSERT [dbo].[SousCategories] ([idCategorie],[nom]) VALUES (1,'Couvert')
-INSERT [dbo].[SousCategories] ([idCategorie],[nom]) VALUES (1,'Linge de table')
-INSERT [dbo].[SousCategories] ([idCategorie],[nom]) VALUES (1,'Accessoires')
-	
-INSERT [dbo].[SousCategories] ([idCategorie],[nom]) VALUES (2,'Corbeilles')
-INSERT [dbo].[SousCategories] ([idCategorie],[nom]) VALUES (2,'Vases')
-INSERT [dbo].[SousCategories] ([idCategorie],[nom]) VALUES (2,'Boîtes')
-INSERT [dbo].[SousCategories] ([idCategorie],[nom]) VALUES (2,'Bureau et papeterie')
-INSERT [dbo].[SousCategories] ([idCategorie],[nom]) VALUES (2,'Petit mobilier')
-INSERT [dbo].[SousCategories] ([idCategorie],[nom]) VALUES (2,'Tentures')
-INSERT [dbo].[SousCategories] ([idCategorie],[nom]) VALUES (2,'Statues et figurines')
-INSERT [dbo].[SousCategories] ([idCategorie],[nom]) VALUES (2,'Décorations de Noël')
-
-INSERT [dbo].[SousCategories] ([idCategorie],[nom]) VALUES (3,'Colliers et médailles')
-INSERT [dbo].[SousCategories] ([idCategorie],[nom]) VALUES (3,'Broches et barrettes')
-INSERT [dbo].[SousCategories] ([idCategorie],[nom]) VALUES (3,'Bagues')
-INSERT [dbo].[SousCategories] ([idCategorie],[nom]) VALUES (3,'Bracelets')
-INSERT [dbo].[SousCategories] ([idCategorie],[nom]) VALUES (3,'Boucles d''oreilles')
-INSERT [dbo].[SousCategories] ([idCategorie],[nom]) VALUES (3,'Accessoires')
-
-
-
-
-INSERT [dbo].[SousCategories] ([idCategorie],[nom]) VALUES (4,'Sacs et paniers')
-INSERT [dbo].[SousCategories] ([idCategorie],[nom]) VALUES (4,'Maroquinerie')
-INSERT [dbo].[SousCategories] ([idCategorie],[nom]) VALUES (4,'Trousses et étuis')
-INSERT [dbo].[SousCategories] ([idCategorie],[nom]) VALUES (4,'Porte-clefs')
-
-
-INSERT [dbo].[SousCategories] ([idCategorie],[nom]) VALUES (5,'Bougies')
-INSERT [dbo].[SousCategories] ([idCategorie],[nom]) VALUES (5,'Photophore et bougeoirs')
-INSERT [dbo].[SousCategories] ([idCategorie],[nom]) VALUES (5,'Encens et supports')
-INSERT [dbo].[SousCategories] ([idCategorie],[nom]) VALUES (5,'Bien-être')
-
-
-INSERT [dbo].[SousCategories] ([idCategorie],[nom]) VALUES (6,'Mode femme')
-INSERT [dbo].[SousCategories] ([idCategorie],[nom]) VALUES (6,'Mode homme')
-INSERT [dbo].[SousCategories] ([idCategorie],[nom]) VALUES (6,'Mode enfant')
-INSERT [dbo].[SousCategories] ([idCategorie],[nom]) VALUES (6,'Echarpes et etoles')
-INSERT [dbo].[SousCategories] ([idCategorie],[nom]) VALUES (6,'Accessoires')
-
-INSERT [dbo].[SousCategories] ([idCategorie],[nom]) VALUES (7,'Jeux')
-INSERT [dbo].[SousCategories] ([idCategorie],[nom]) VALUES (7,'Jouets en bois')
-INSERT [dbo].[SousCategories] ([idCategorie],[nom]) VALUES (7,'Instruments')
-INSERT [dbo].[SousCategories] ([idCategorie],[nom]) VALUES (7,'Poupées et animaux')
-INSERT [dbo].[SousCategories] ([idCategorie],[nom]) VALUES (7,'Mobiles')
-INSERT [dbo].[SousCategories] ([idCategorie],[nom]) VALUES (7,'Décoration')
-
-INSERT [dbo].[SousCategories] ([idCategorie],[nom]) VALUES (8,'Divers')
-
-
-
-
-
-
-
-
-
-
-
-
+INSERT [dbo].[Photos] ([PhotoName], [IdProduit], [IdPhoto]) VALUES (N'Need Code By PCbots.png', 1, 1)
+INSERT [dbo].[Photos] ([PhotoName], [IdProduit], [IdPhoto]) VALUES (N'SiliconeValley400_300.jpg', 1, 2)
+INSERT [dbo].[Photos] ([PhotoName], [IdProduit], [IdPhoto]) VALUES (N'acp417.jpg', 2, 3)
+INSERT [dbo].[Photos] ([PhotoName], [IdProduit], [IdPhoto]) VALUES (N'clk029.jpg', 3, 4)
+INSERT [dbo].[Photos] ([PhotoName], [IdProduit], [IdPhoto]) VALUES (N'shutterstock_197353127_6.jpg', 3, 5)
+INSERT [dbo].[Photos] ([PhotoName], [IdProduit], [IdPhoto]) VALUES (N'GPH025_1.jpg', 4, 6)
+INSERT [dbo].[Photos] ([PhotoName], [IdProduit], [IdPhoto]) VALUES (N'MAI134_3.jpg', 5, 7)
+INSERT [dbo].[Photos] ([PhotoName], [IdProduit], [IdPhoto]) VALUES (N'MAI289_1.jpg', 6, 8)
+INSERT [dbo].[Photos] ([PhotoName], [IdProduit], [IdPhoto]) VALUES (N'SIL200_2.jpg', 8, 9)
+INSERT [dbo].[Photos] ([PhotoName], [IdProduit], [IdPhoto]) VALUES (N'mai352.jpg', 10, 10)
+INSERT [dbo].[Photos] ([PhotoName], [IdProduit], [IdPhoto]) VALUES (N'shutterstock_97516112.jpg', 10, 11)
+INSERT [dbo].[Photos] ([PhotoName], [IdProduit], [IdPhoto]) VALUES (N'shutterstock_136795478.jpg', 10, 12)
+INSERT [dbo].[Photos] ([PhotoName], [IdProduit], [IdPhoto]) VALUES (N'MAI134_3.jpg', 9, 13)
+INSERT [dbo].[Photos] ([PhotoName], [IdProduit], [IdPhoto]) VALUES (N'shutterstock_197353127_6.jpg', 9, 14)
+SET IDENTITY_INSERT [dbo].[Photos] OFF
 
 GO
 ALTER TABLE [dbo].[Administrateurs]  WITH CHECK ADD  CONSTRAINT [fk_administrateurs_identites] FOREIGN KEY([IdAdministrateur])
--- Form113/Controllers/CommandeController.cs
+++ Form113/Controllers/CommandeController.cs
@@ -1,4 +1,5 @@
-﻿using Form113.Models;
+﻿using DataLayer.Models;
+using Form113.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -9,6 +10,7 @@ namespace Form113.Controllers
 {
     public class CommandeController : BestArtController
     {
+        private static BestArtEntities db = new BestArtEntities();
 
         public CommandeController()
         {
@@ -26,7 +28,38 @@ namespace Form113.Controllers
         public ActionResult Payment(CommandeViewModels cvm)
         {
             var str = cvm.ListeProduitCommande;
+            var liste = str.Split('/');
+            var listeRes = new List<KeyValuePair<int, int>>();
+            foreach (var item in liste)
+            {
+                var tempo = item.Split(',');
+                var kvp = new KeyValuePair<int, int>(int.Parse(tempo[0]), int.Parse(tempo[1]));
+                listeRes.Add(kvp);
+            }
 
+            var iduserASP = db.AspNetUsers.Where(x => x.Email == User.Identity.Name).Select(x => x.Id).FirstOrDefault();
+            int iduser = db.Utilisateurs.Where(x => x.IdAsp == iduserASP).Select(x => x.IdUtilisateur).FirstOrDefault();
+            var Commande = new Commandes()
+            {
+                DateCommande = DateTime.Now,
+                EtatCommande = "0",
+                IdAcheteur = iduser,
+                IdAdresse = db.Utilisateurs.Where(u => u.IdUtilisateur == iduser).Select(x => x.IdAdresse).FirstOrDefault(),
+            };
+
+            foreach (var item in listeRes)
+            {
+                var OrderDetail = new Commandes_details()
+                {
+                    IdProduit = item.Key,
+                    quantite = item.Value,
+                    Promotion = db.Produits.Where(p => p.IdProduit == item.Key).Select(p => p.Promotion).FirstOrDefault(),
+                    prix_unitaire = db.Produits.Where(p => p.IdProduit == item.Key).Select(p => p.Prix).FirstOrDefault(),
+                };
+                Commande.Commandes_details.Add(OrderDetail);
+            }
+            db.Commandes.Add(Commande);
+            db.SaveChanges();
             return View("Result");
         }
 
