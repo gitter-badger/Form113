e14892773de04495f6d1fc2e2e3803507ccd5649
Junqua <greg.junqua@gmail.com>
2015-09-03 10:21:33 +0200

    Update Etat commande

---
+++ Documents/ScriptsSQL/EtatCommande.sql
@@ -0,0 +1,27 @@
+use form113
+go
+drop table "StatusCommande";
+go
+create table StatusCommande (
+	IdStatusCommande int Primary key not null,
+	StatusCommande nvarchar(30) not null
+)
+insert StatusCommande (IdStatusCommande,StatusCommande)
+values (1,'Commandé')
+insert StatusCommande (IdStatusCommande,StatusCommande)
+values (2,'Envoyé')
+insert StatusCommande (IdStatusCommande,StatusCommande)
+values (3,'Reçu')
+
+go 
+create table Marketing (
+	NbreCommandePourReduc int,
+)
+
+go
+
+Alter table Commandes alter column EtatCommande INT;
+alter table Commandes
+add constraint fk_EtatCommande
+foreign key (EtatCommande)
+references StatusCommande(IdStatusCommande)
