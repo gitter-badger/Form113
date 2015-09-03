0c4e0f47d37e6266f5e2d885a60dc5bf682a0fad
Junqua <greg.junqua@gmail.com>
2015-09-03 15:34:21 +0200

    Index Commande
    Ajout de la gestion de commande

---
+++ DataLayer/Commande.cs
@@ -0,0 +1,73 @@
+﻿using DataLayer.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataLayer
+{
+    /// <summary>
+    /// Class permattant de récuperer les infos commande 
+    /// </summary>
+    public class Commande
+    {
+        #region Public
+        public string Nom;
+        public string Prenom;
+        public DateTime OrderDate;
+        public int OrderAge;
+        public float TotalCommande;
+        public string CommandeStatus;
+        public int IdAcheteur;
+        public int IdCommande;
+        #endregion
+
+        private BestArtEntities db = new BestArtEntities();
+
+        public Commande(Models.Commandes commande)
+        {
+            // Recherche du nom et du prenom
+            var utilisateur = db.Utilisateurs.Find(commande.IdAcheteur);
+            var identities = db.Identites.Find(utilisateur.IdIdentite);
+            Nom = identities.Nom;
+            Prenom = identities.Prenom;
+            // Calcul au niveau de la date
+            OrderDate = commande.DateCommande.GetValueOrDefault(DateTime.Today);
+            OrderAge = (int)(OrderDate-DateTime.Now).TotalDays ;
+
+            // calcul de la somme
+            TotalCommande = giveSummeCommande(commande);
+            CommandeStatus = commande.StatusCommande.StatusCommande1;
+
+            // Recupération des ID important
+            IdAcheteur = utilisateur.IdUtilisateur;
+            IdCommande = commande.IdCommande;
+        }
+
+        private float giveSummeCommande(Models.Commandes commande)
+        {
+            var commandeDetail = db.Commandes_details.Where(c => c.IdCommande == commande.IdCommande).ToList();
+            float sumToMe = 0;
+            foreach(var item in commandeDetail)
+            {
+                var _prix = item.Produits.Prix ;
+                int _nbre = item.quantite ?? 0;
+                sumToMe += (float)(_prix * _nbre);
+            }
+            return sumToMe;
+        }
+        public static List<Commande> giveMeList()
+        {
+            var db = new BestArtEntities();
+            var listCommande = new List<Commande>();
+            var collection = db.Commandes;
+            foreach(var item in collection)
+            {
+                var comm = new Commande(item);
+                listCommande.Add(comm);
+            }
+            return listCommande;
+        }
+    }
+}
---
+++ Form113/Areas/Admin/Controllers/CommandesController.cs
@@ -0,0 +1,139 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using DataLayer.Models;
+
+namespace Form113.Areas.Admin.Controllers
+{
+    public class CommandesController : Controller
+    {
+        private BestArtEntities db = new BestArtEntities();
+
+        // GET: Admin/Commandes
+        public ActionResult Index()
+        {
+            var listCommande = DataLayer.Commande.giveMeList();
+            return View(listCommande);
+            // Automatic code
+            //var commandes = db.Commandes.Include(c => c.Utilisateurs).Include(c => c.StatusCommande);
+            //return View(commandes.ToList());
+        }
+
+        // GET: Admin/Commandes/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Commandes commandes = db.Commandes.Find(id);
+            if (commandes == null)
+            {
+                return HttpNotFound();
+            }
+            return View(commandes);
+        }
+
+        // GET: Admin/Commandes/Create
+        public ActionResult Create()
+        {
+            ViewBag.IdAcheteur = new SelectList(db.Utilisateurs, "IdUtilisateur", "IdAsp");
+            ViewBag.EtatCommande = new SelectList(db.StatusCommande, "IdStatusCommande", "StatusCommande1");
+            return View();
+        }
+
+        // POST: Admin/Commandes/Create
+        // Afin de déjouer les attaques par sur-validation, activez les propriétés spécifiques que vous voulez lier. Pour 
+        // plus de détails, voir  http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "IdCommande,IdAcheteur,EtatCommande,DateCommande,DateLivraison,IdAdresse")] Commandes commandes)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Commandes.Add(commandes);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.IdAcheteur = new SelectList(db.Utilisateurs, "IdUtilisateur", "IdAsp", commandes.IdAcheteur);
+            ViewBag.EtatCommande = new SelectList(db.StatusCommande, "IdStatusCommande", "StatusCommande1", commandes.EtatCommande);
+            return View(commandes);
+        }
+
+        // GET: Admin/Commandes/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Commandes commandes = db.Commandes.Find(id);
+            if (commandes == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.IdAcheteur = new SelectList(db.Utilisateurs, "IdUtilisateur", "IdAsp", commandes.IdAcheteur);
+            ViewBag.EtatCommande = new SelectList(db.StatusCommande, "IdStatusCommande", "StatusCommande1", commandes.EtatCommande);
+            return View(commandes);
+        }
+
+        // POST: Admin/Commandes/Edit/5
+        // Afin de déjouer les attaques par sur-validation, activez les propriétés spécifiques que vous voulez lier. Pour 
+        // plus de détails, voir  http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "IdCommande,IdAcheteur,EtatCommande,DateCommande,DateLivraison,IdAdresse")] Commandes commandes)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(commandes).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.IdAcheteur = new SelectList(db.Utilisateurs, "IdUtilisateur", "IdAsp", commandes.IdAcheteur);
+            ViewBag.EtatCommande = new SelectList(db.StatusCommande, "IdStatusCommande", "StatusCommande1", commandes.EtatCommande);
+            return View(commandes);
+        }
+
+        // GET: Admin/Commandes/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Commandes commandes = db.Commandes.Find(id);
+            if (commandes == null)
+            {
+                return HttpNotFound();
+            }
+            return View(commandes);
+        }
+
+        // POST: Admin/Commandes/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Commandes commandes = db.Commandes.Find(id);
+            db.Commandes.Remove(commandes);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
