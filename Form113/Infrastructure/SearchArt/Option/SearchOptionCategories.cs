97ce3f699ac7f203287c1cf747a1cb3ace919f06
Junqua <greg.junqua@gmail.com>
2015-09-03 16:51:36 +0200

    search option categorie souscategories

---
+++ Form113/Infrastructure/SearchArt/Option/SearchOptionCategories.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Form113.Infrastructure.SearchArt.Option
+{
+    #region UsingReg
+    using System.Collections.Generic;
+    using System.Linq;
+    using DataLayer.Models;
+    using Form113.Infrastructure.SearchArt.Base;
+    #endregion
+
+    internal class SearchOptionCategories : SearchOption
+    {
+        private readonly int [] _SelectedCategories;
+
+        public SearchOptionCategories(SearchBase sb, int[] listCategories) : base(sb)
+        {
+            _SelectedCategories = listCategories;
+        }
+
+        public override IEnumerable<Produits> GetResult()
+        {
+            if (_SelectedCategories== null || _SelectedCategories.Count()<=0)
+            {
+                return SearchBase.GetResult();
+            }
+            else
+            {
+                return SearchBase.GetResult().Where(x => _SelectedCategories.ToArray().Contains(x.SousCategories.Categories.IdCategorie)).OrderBy(x => x.Nom);
+            }
+        }
+    }
+}
---
+++ Form113/Infrastructure/SearchArt/Option/SearchOptionSousCategories.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Form113.Infrastructure.SearchArt.Option
+{
+    #region UsingReg
+    using System.Collections.Generic;
+    using System.Linq;
+    using DataLayer.Models;
+    using Form113.Infrastructure.SearchArt.Base;
+    #endregion
+
+    internal class SearchOptionSousCategories : SearchOption
+    {
+        private readonly int[] _SelectedSousCategories;
+
+        public SearchOptionSousCategories(SearchBase sb,int[] listeSousCategories) : base(sb)
+        {
+            _SelectedSousCategories = listeSousCategories;
+        }
+        public override IEnumerable<Produits> GetResult()
+        {
+            if (_SelectedSousCategories==null || _SelectedSousCategories.Count() <=0)
+            {
+                return SearchBase.GetResult();
+            }
+            else
+            {
+                return SearchBase.GetResult().Where(x => _SelectedSousCategories.ToArray().Contains(x.SousCategories.IdSousCategorie)).OrderBy(x => x.Nom);
+            }
+        }
+
+    }
+}
