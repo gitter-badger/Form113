8524f22b638ac193d2b01a5661c0886865e329a2
Junqua <greg.junqua@gmail.com>
2015-09-04 09:13:15 +0200

    SearchOption Ajouté

---
+++ Form113/Infrastructure/SearchArt/Option/SearchOptionPromotions.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Form113.Infrastructure.SearchArt.Option
+{
+    #region UsingReg
+    using System.Collections.Generic;
+    using System.Linq;
+    using DataLayer.Models;
+    using Form113.Infrastructure.SearchArt.Base;
+    #endregion
+
+    internal class SearchOptionPromotions : SearchOption
+    {
+        private readonly int _Promotion;
+
+            public SearchOptionPromotions (SearchBase sb,int promotion) : base(sb)
+        {
+            _Promotion = promotion;
+        }
+            public override IEnumerable<Produits> GetResult()
+            {
+                return SearchBase.GetResult().Where(x => (1 - x.Promotion) < _Promotion);
+            }
+    }
+}
