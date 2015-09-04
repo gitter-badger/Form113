0f36fd110113d1404c070460b7f3a260b7cbf6c3
Junqua <greg.junqua@gmail.com>
2015-09-04 16:24:20 +0200

    NewsLetter style simple
    Ajout de plus de module

---
+++ Form113/Areas/Admin/Models/newsLetterViewModel.cs
@@ -0,0 +1,17 @@
+﻿using DataLayer.Models;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Form113.Areas.Admin.Models
+{
+    public class newsLetterViewModel
+    {
+        public string Title { get; set; }
+        public string Description { get; set; }
+    }
+}
